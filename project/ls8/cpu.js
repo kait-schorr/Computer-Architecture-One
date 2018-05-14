141e729a84512cc3825b392baeae846fb7d735f7
kait-schorr <kaitlynschorr@gmail.com>
2018-05-14 17:45:39 -0500

    Got print8 to work, started working on getting a file from the command line

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -6,91 +6,132 @@
  * Class for simulating a simple Computer (CPU & memory)
  */
 class CPU {
+  /**
+   * Initialize the CPU
+   */
+  constructor(ram) {
+    this.ram = ram;
 
-    /**
-     * Initialize the CPU
-     */
-    constructor(ram) {
-        this.ram = ram;
+    this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
 
-        this.reg = new Array(8).fill(0); // General-purpose registers R0-R7
-        
-        // Special-purpose registers
-        this.PC = 0; // Program Counter
+    // Special-purpose registers
+    this.PC = 0; // Program Counter
+  }
+  binaryToDecimal(bin) {
+    return parseInt(bin, 10);
+  }
+  decimalToBinary(dec) {
+    let output = "";
+    let length = 8;
+    while (length--) {
+      output += (dec >> length) & 1;
     }
-    
-    /**
-     * Store value in memory address, useful for program loading
-     */
-    poke(address, value) {
-        this.ram.write(address, value);
-    }
+    return output;
+  }
+  /**
+   * Store value in memory address, useful for program loading
+   */
+  poke(address, value) {
+    this.ram.write(address, value);
+  }
 
-    /**
-     * Starts the clock ticking on the CPU
-     */
-    startClock() {
-        this.clock = setInterval(() => {
-            this.tick();
-        }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
-    }
+  /**
+   * Starts the clock ticking on the CPU
+   */
+  startClock() {
+    this.clock = setInterval(() => {
+      this.tick();
+    }, 1); // 1 ms delay == 1 KHz clock == 0.000001 GHz
+  }
 
-    /**
-     * Stops the clock
-     */
-    stopClock() {
-        clearInterval(this.clock);
-    }
+  /**
+   * Stops the clock
+   */
+  stopClock() {
+    clearInterval(this.clock);
+  }
 
-    /**
-     * ALU functionality
-     *
-     * The ALU is responsible for math and comparisons.
-     *
-     * If you have an instruction that does math, i.e. MUL, the CPU would hand
-     * it off to it's internal ALU component to do the actual work.
-     *
-     * op can be: ADD SUB MUL DIV INC DEC CMP
-     */
-    alu(op, regA, regB) {
-        switch (op) {
-            case 'MUL':
-                // !!! IMPLEMENT ME
-                break;
-        }
-    }
-
-    /**
-     * Advances the CPU one cycle
-     */
-    tick() {
-        // Load the instruction register (IR--can just be a local variable here)
-        // from the memory address pointed to by the PC. (I.e. the PC holds the
-        // index into memory of the instruction that's about to be executed
-        // right now.)
-
+  hlt() {
+    this.stopClock();
+    return;
+  }
+  ldi(register, integer) {
+    this.reg[this.binaryToDecimal(register)] = integer;
+  }
+  /**
+   * ALU functionality
+   *
+   * The ALU is responsible for math and comparisons.
+   *
+   * If you have an instruction that does math, i.e. MUL, the CPU would hand
+   * it off to it's internal ALU component to do the actual work.
+   *
+   * op can be: ADD SUB MUL DIV INC DEC CMP
+   */
+  prn(register) {
+    console.log(this.binaryToDecimal(this.reg[this.binaryToDecimal(register)]));
+  }
+  alu(op, regA, regB) {
+    switch (op) {
+      case "MUL":
         // !!! IMPLEMENT ME
-
-        // Debugging output
-        //console.log(`${this.PC}: ${IR.toString(2)}`);
-
-        // Get the two bytes in memory _after_ the PC in case the instruction
-        // needs them.
-
-        // !!! IMPLEMENT ME
-
-        // Execute the instruction. Perform the actions for the instruction as
-        // outlined in the LS-8 spec.
-
-        // !!! IMPLEMENT ME
-
-        // Increment the PC register to go to the next instruction. Instructions
-        // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
-        // instruction byte tells you how many bytes follow the instruction byte
-        // for any particular instruction.
-        
-        // !!! IMPLEMENT ME
+        this.reg[regA] = this.reg[regA] * this.reg[regB];
+        break;
     }
+  }
+
+  /**
+   * Advances the CPU one cycle
+   */
+  tick() {
+    // Load the instruction register (IR--can just be a local variable here)
+    // from the memory address pointed to by the PC. (I.e. the PC holds the
+    // index into memory of the instruction that's about to be executed
+    // right now.)
+    let IR = this.ram.read(this.PC);
+    // !!! IMPLEMENT ME
+
+    // Debugging output
+    console.log(`${this.PC}: ${IR.toString(2)}`);
+
+    // Get the two bytes in memory _after_ the PC in case the instruction
+    // needs them.
+
+    // !!! IMPLEMENT ME
+    let next1 = this.ram.read(this.PC + 1);
+    let next2 = this.ram.read(this.PC + 2);
+
+    // Execute the instruction. Perform the actions for the instruction as
+    // outlined in the LS-8 spec.
+
+    switch (this.decimalToBinary(IR)) {
+      case "10011001":
+        this.ldi(next1, next2);
+        break;
+      case "01000011":
+        this.prn(next1);
+        break;
+      case "00000001":
+        this.hlt();
+        break;
+      case "10101010":
+        alu("MUL", next1, next2);
+        break;
+      default:
+        this.hlt();
+    }
+
+    // !!! IMPLEMENT ME
+
+    // Increment the PC register to go to the next instruction. Instructions
+    // can be 1, 2, or 3 bytes long. Hint: the high 2 bits of the
+    // instruction byte tells you how many bytes follow the instruction byte
+    // for any particular instruction.
+    let increment = parseInt(this.decimalToBinary(IR).slice(0, 2), 2) + 1;
+    this.PC += increment;
+
+    // !!! IMPLEMENT ME
+  }
 }
 
 module.exports = CPU;
--- project/ls8/ls8.js
+++ project/ls8/ls8.js
@@ -1,5 +1,6 @@
-const RAM = require('./ram');
-const CPU = require('./cpu');
+const readline = require("readline");
+const RAM = require("./ram");
+const CPU = require("./cpu");
 
 /**
  * Load an LS8 program into memory
@@ -7,22 +8,22 @@ const CPU = require('./cpu');
  * TODO: load this from a file on disk instead of having it hardcoded
  */
 function loadMemory() {
+  // Hardcoded program to print the number 8 on the console
 
-    // Hardcoded program to print the number 8 on the console
+  const program = [
+    // print8.ls8
+    "10011001", // LDI R0,8  Store 8 into R0
+    "00000000",
+    "00001000",
+    "01000011", // PRN R0    Print the value in R0
+    "00000000",
+    "00000001" // HLT       Halt and quit
+  ];
 
-    const program = [ // print8.ls8
-        "10011001", // LDI R0,8  Store 8 into R0
-        "00000000",
-        "00001000",
-        "01000011", // PRN R0    Print the value in R0
-        "00000000",
-        "00000001"  // HLT       Halt and quit
-    ];
-
-    // Load the program into the CPU's memory a byte at a time
-    for (let i = 0; i < program.length; i++) {
-        cpu.poke(i, parseInt(program[i], 2));
-    }
+  // Load the program into the CPU's memory a byte at a time
+  for (let i = 0; i < program.length; i++) {
+    cpu.poke(i, parseInt(program[i], 2));
+  }
 }
 
 /**
@@ -33,6 +34,8 @@ let ram = new RAM(256);
 let cpu = new CPU(ram);
 
 // TODO: get name of ls8 file to load from command line
+let file = process.argv[3];
+console.log(file);
 
 loadMemory(cpu);
 
